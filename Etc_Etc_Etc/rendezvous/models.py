7191132682c790658825351ed1da6f7fa9994093
LOCosta <lcosta@umass.edu>
2017-10-24 14:03:47 -0400

    Adds skeleton for data model

--- Etc_Etc_Etc/rendezvous/models.py
+++ Etc_Etc_Etc/rendezvous/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.urls import reverse
+import uuid
+
 
 # Create your models here.
+class Skill(models.Model):
+    pass
+
+
+class Tag(models.Model):
+    pass
+
+
+class User(models.Model):
+    pass
+
+
+class Project(models.Model):
+    pass
+
+
+class Location(models.Model):
+    pass
+
+
+class Event(models.Model):
+    pass
