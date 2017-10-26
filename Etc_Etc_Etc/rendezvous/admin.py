05de9a9b41fa7637583e59140ff47649772a82eb
BlacKitten <sand.warrior@hotmail.com>
2017-10-26 18:56:03 -0400

    Admin Declarations

--- Etc_Etc_Etc/rendezvous/admin.py
+++ Etc_Etc_Etc/rendezvous/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Skill, Tag, Event, Location, User, Project
+
+admin.site.register(Skill)
+admin.site.register(Tag)
+admin.site.register(Event)
+admin.site.register(Location)
+admin.site.register(User)
+admin.site.register(Project)
+
