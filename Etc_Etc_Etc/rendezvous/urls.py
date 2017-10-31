d04a459112f315f38012979a079121c749d545f3
LOCosta <lcosta@umass.edu>
2017-10-31 17:50:43 -0400

    Fixes variety of errors that were keeping any of the pages from loading, and updates some styling.

--- Etc_Etc_Etc/rendezvous/urls.py
+++ Etc_Etc_Etc/rendezvous/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
 
 from . import views
-
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'create-project/', views.CREATE-PROJECT, name='create-project'),
-    url(r'project/(?P<id>\d+)/$', views.PROJECT, name='view-project'),
-    url(r'user/(?P<id>\d+)/$', views.USER, name='view-user-profile'),
+    url(r'create-project/', views.create_project, name='create-project'),
+    url(r'project/(?P<id>[\d\w]+)/$', views.project, name='view-project'),
+    # url(r'user/', views.profile, name='view-current-user-profile'),  # Requires sessions to be implemented
+    url(r'user/(?P<id>[\d\w]+)/$', views.profile, name='view-user-profile'),
     url(r'advanced-search/', views.search, name='advanced-search',),
 ]
--- Etc_Etc_Etc/rendezvous/views.py
+++ Etc_Etc_Etc/rendezvous/views.py
@@ -1,25 +1,28 @@
 from django.shortcuts import render
 from .models import Skill, Tag, Event, Location, User, Project
 
+
 def profile(request, id):
     """
     View function for the profile page.
     """
     user = User.objects.get(id=id)
     user_name = user.name
-    user_skills = user.relevant_skills
+    user_skills = user.relevant_skills.all()
 
     proj_created = user.projects_owned.order_by('-date_created')
     num_proj_created = proj_created.count()
 
-    proj_contributed = user.projects_contributed.order_by('-date_joined')
+    proj_contributed = user.project_set.all()
     num_proj_contributed = proj_contributed.count()
 
     return render(
         request, 'profile.html',
         context={'user_name': user_name, 'user_skills': user_skills, 'proj_created': proj_created,
-                 'num_proj_created': num_proj_created, 'num_proj_contributed': num_proj_contributed}
+                 'num_proj_created': num_proj_created, 'proj_contributed': proj_contributed,
+                 'num_proj_contributed': num_proj_contributed}
     )
+
 
 def project(request, id):
     """
@@ -29,8 +32,8 @@ def project(request, id):
     proj_name = project.name
     proj_creator = project.owner
     proj_description = project.description
-    proj_contributors = project.contributors
-    skills_desired = project.skills_desired
+    proj_contributors = project.contributors.all()
+    skills_desired = project.skills_desired.all()
 
     return render(
         request, 'project.html',
@@ -41,13 +44,17 @@ def project(request, id):
 
 def index(request):
     return render(
-    request, 'index.html',
-    context={})
+        request, 'index.html',
+        context={}
+    )
+
 
 def create_project(request):
-    return render (
-    request, 'create-a-new-project-post.html',
-    context={})
+    skills = Skill.objects.all()
+    return render(
+        request, 'create-a-new-project-post.html',
+        context={'skills': skills}
+    )
 
 
 def search(request):
@@ -55,8 +62,9 @@ def search(request):
     View function for advanced search page.
     """
 
-    result_rows = Project.objects.all()[:12]  # Load only up to 16 results for now, need more after pages to search
-    skills = Skill.ojects.all()
+    projects = Project.objects.all()[:12]  # Load only up to 12 results for now, need more once search is implemented
+    skills = Skill.objects.all()
+    result_rows = [projects[0:4], projects[4:8], projects[8:12]]  # Also temporary.
 
     return render(
         request,
