eede7311b8c4d0eb7d13ce64077b4ddb29b0393b
LOCosta <lcosta@umass.edu>
2017-10-31 11:04:53 -0400

    Updated url mappings for base site and added url mappings for webapp

--- Etc_Etc_Etc/Etc_Etc_Etc/urls.py
+++ Etc_Etc_Etc/Etc_Etc_Etc/urls.py
@@ -22,6 +22,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^rendezvous', include('rendezvous.urls')),
+    url(r'^rendezvous/', include('rendezvous.urls')),
     url(r'^$', RedirectView.as_view(url='/rendezvous/', permanent=True))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- Etc_Etc_Etc/rendezvous/urls.py
+++ Etc_Etc_Etc/rendezvous/urls.py
@@ -5,4 +5,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'create-project/', views.CREATE-PROJECT, name='create-project'),
+    url(r'project/(?P<id>\d+)/$', views.PROJECT, name='view-project'),
+    url(r'user/(?P<id>\d+)/$', views.USER, name='view-user-profile'),
+    url(r'advanced-search/', views.search, name='advanced-search',),
 ]
